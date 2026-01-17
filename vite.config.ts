1f57615e1374de590b246616920047adbfbf0f45
Leny Diallo <leny@demandealeny.com>
2026-01-17 18:30:31 +0100

    Update Vite config to remove Vercel plugin
    Removed Vercel plugin from Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: process.env.PORT || 8080,
-      },
-      plugins: [react(), vercel()],
+      plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
