b31d474c77c57175a3a740ae627dc357427bb7f1
Leny Diallo <leny@demandealeny.com>
2026-01-18 12:43:37 +0100

    feat: Configure Astro for server-side rendering using the Node.js adapter and define the site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  site: 'https://audio.leny.store',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react()],
   vite: {
     define: {
